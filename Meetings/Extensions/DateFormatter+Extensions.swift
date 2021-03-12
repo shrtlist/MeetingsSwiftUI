86a739a3c92a06f5177fdd02aa301fcb2dc6e0a6
Marco Abundo <shrtlist@gmail.com>
2021-03-12 14:35:46 -0800

    Removed unused date formatter

--- Meetings/Extensions/DateFormatter+Extensions.swift
+++ Meetings/Extensions/DateFormatter+Extensions.swift
@@ -10,27 +10,12 @@ import Foundation
 public extension DateFormatter {
 
     private struct CachedFormatters {
-        static var iso8601UTCDateFormatter: ISO8601DateFormatter = {
-            let formatter = ISO8601DateFormatter()
-            formatter.formatOptions = [.withInternetDateTime,
-                                       .withDashSeparatorInDate,
-                                       .withFullDate,
-                                       .withFractionalSeconds,
-                                       .withColonSeparatorInTimeZone]
-            return formatter
-        }()
-
         static var mediumDateFormatter: DateFormatter = {
             let formatter = DateFormatter()
             formatter.dateStyle = .medium
             formatter.dateFormat = .none
             return formatter
         }()
-    }
-
-    /// Shared instance of `ISO8601DateFormatter`
-    static var iso8601UTCDateFormatter: ISO8601DateFormatter {
-        return CachedFormatters.iso8601UTCDateFormatter
     }
 
     /// Shared instance of the date formatter with short date format
